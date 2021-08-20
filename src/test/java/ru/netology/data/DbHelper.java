b762213f99720eae64892f900c3f1e8ded700ea4
Gleb <demonxklimenko@mail.ru>
2021-08-20 08:46:03 +0300

    Fix

--- src/test/java/ru/netology/data/DbHelper.java
+++ src/test/java/ru/netology/data/DbHelper.java
@@ -4,57 +4,53 @@ import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
+import java.sql.SQLException;
+import java.sql.*;
+
+
 
 public class DbHelper {
     private final QueryRunner runner = new QueryRunner();
-    private Properties prop = prop();
-    private final Connection conn = getConnect();
+    public static Connection connection;
+    public static Statement statement;
 
+    public static String url = System.getProperty("property.URL");
+    public static String user = System.getProperty("property.NAME_USER");
+    public static String password = System.getProperty("property.PASSWORD");
 
-    private Properties prop() {
-        Properties properties = new Properties();
-        try (InputStream is = DbHelper.class.getClassLoader().getResourceAsStream("application.properties")) {
-            properties.load(is);
-        } catch(IOException ex) { ex.printStackTrace(); }
-        return properties;
-    }
-
-
-    @SneakyThrows
-    private Connection getConnect() {
-        return DriverManager.getConnection(
-                prop.getProperty("spring.datasource.url"),
-                prop.getProperty("spring.datasource.username"),
-                prop.getProperty("spring.datasource.password")
-        );
+    static {
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+        } catch (
+                SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @SneakyThrows
     public String getPaymentStatus() {
         var status = "SELECT status FROM payment_entity ORDER BY created DESC";
-        return runner.query(conn, status, new ScalarHandler<>());
+        return runner.query(connection, status, new ScalarHandler<>());
     }
 
     @SneakyThrows
     public Integer getPaymentAmount() {
         var amount = "SELECT amount FROM payment_entity ORDER BY created DESC";
-        return runner.query(conn, amount, new ScalarHandler<>());
+        return runner.query(connection, amount, new ScalarHandler<>());
     }
 
     @SneakyThrows
     public String getCreditRequestStatus() {
         var status = "SELECT status FROM credit_request_entity ORDER BY created DESC";
-        return runner.query(conn, status, new ScalarHandler<>());
+        return runner.query(connection, status, new ScalarHandler<>());
     }
 
     @SneakyThrows
     public String getCreditId() {
         var id = "SELECT credit_id FROM order_entity ORDER BY created DESC";
-        return runner.query(conn, id, new ScalarHandler<>());
+        return runner.query(connection, id, new ScalarHandler<>());
     }
 }
