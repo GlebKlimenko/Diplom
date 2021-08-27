ae04dd5917d73e052ead55622623f5dcec0009ae
Gleb <demonxklimenko@mail.ru>
2021-08-27 15:54:37 +0300

    Fix connect MySQL and PostgreSQL

--- src/test/java/ru/netology/test/ByinCreditTest.java
+++ src/test/java/ru/netology/test/ByinCreditTest.java
@@ -2,10 +2,7 @@ package ru.netology.test;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import ru.netology.data.DataHelper;
 import ru.netology.data.DbHelper;
 import ru.netology.page.OrderPage;
